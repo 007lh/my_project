bb2551a8efb1a78a2a7717cf17a83d0b1bedca09
007lh <1597302572@qq.com>
2019-03-19 21:48:42 +0800

    admin
    admin upload frame changed

--- musicReact/app/page/admin.js
+++ musicReact/app/page/admin.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { browserHistory } from 'react-router'
+import '../../static/css/admin.css'
 //import mongoose from 'mongoose'
 //import MusicsSchema from './admin/schemas'
 //var Musics = mongoose.model('Musics', MusicsSchema)
@@ -63,59 +64,59 @@ class Admin extends React.Component {
   render(){
     return (
     <div>
-      <div>后台录入页</div>
-        <div className="container">
+      <div className="centerword">后台录入页</div>
+        <div className="container textframe">
           <input type="hidden" name="music[_id]"/>
           <form method="post" action="http://localhost:9000" onSubmit={this.handleSubmit}>
-            <div className="row">
-              <label htmlFor="inputTitle" className="control-label">歌曲名字</label>
+            <div className="samllframe">
+              <label htmlFor="inputTitle" className="control-label" className="lab">歌曲名字</label>
               <div className="-col-auto">
                 <input id="inputTitle" type="text"  name="music[title]" onChange={this.handleChange.bind(this,'title')} value={this.state.title}/>
               </div>
             </div>
-            <div className="row">
-              <label htmlFor="inputDoctor" >歌手</label>
+            <div className="samllframe">
+              <label htmlFor="inputDoctor" className="lab">歌手</label>
               <div className="col-sm-10">
                 <input id="inputDoctor" type="text"  name="music[artist]" onChange={this.handleChange.bind(this,'artist')} value={this.state.artist}/>
               </div>
             </div>
-            <div className="row">
-              <label htmlFor="inputCountry" >歌手の国家</label>
+            <div className="samllframe">
+              <label htmlFor="inputCountry" className="lab">歌手の国家</label>
               <div className="col-sm-10">
                 <input id="inputCountry" type="text"  name="music[country]" onChange={this.handleChange.bind(this,'country')} value={this.state.country}/>
               </div>
             </div>
-            <div className="row">
-              <label htmlFor="inputLanguage" >语种</label>
+            <div className="samllframe">
+              <label htmlFor="inputLanguage" className="lab">语种</label>
               <div className="col-sm-10">
                 <input id="inputLanguage" type="text"  name="music[language]" onChange={this.handleChange.bind(this,'language')} value={this.state.language}/>
               </div>
             </div>
-            <div className="row">
-              <label htmlFor="inputPoster" >专辑封面</label>
+            <div className="samllframe">
+              <label htmlFor="inputPoster" className="lab">专辑封面</label>
               <div className="col-sm-10">
                 <input id="inputPoster" type="text"  name="music[cover]" onChange={this.handleChange.bind(this,'cover')} value={this.state.cover}/>
               </div>
             </div>
-            <div className="row">
-              <label htmlFor="inputFlash" >音乐地址</label>
+            <div className="samllframe">
+              <label htmlFor="inputFlash" className="lab">音乐地址</label>
               <div className="col-sm-10">
                 <input id="inputFlash" type="text"  name="music[file]" onChange={this.handleChange.bind(this,'file')} value={this.state.file}/>
               </div>
             </div>
-            <div className="row">
-              <label htmlFor="inputYear" >出版年代</label>
+            <div className="samllframe">
+              <label htmlFor="inputYear" className="lab">出版年代</label>
               <div className="col-sm-10">
                 <input id="inputYear" type="text"  name="music[year]" onChange={this.handleChange.bind(this,'file')} value={this.state.year}/>
               </div>
             </div>
-            <div className="row">
-              <label htmlFor="inputSummary" >作者简介</label>
+            <div className="samllframe">
+              <label htmlFor="inputSummary" className="lab">作者简介</label>
               <div className="col-sm-10">
                 <input id="inputSummary" type="text"  name="music[summary]" onChange={this.handleChange.bind(this,'summary')} value={this.state.summary}/>
               </div>
             </div>
-            <div className="row">
+            <div className="centerword">
               <div className="col-sm-offset-2 col-sm-10"></div>
                 <button type="submit" className="btn btn-default">录入</button>
               </div>
